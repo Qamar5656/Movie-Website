3382e3d4971481e959e17b34734f23128eb7d75f
Qamar5656 <qamar4157@gmail.com>
2025-07-16 12:08:37 +0500

    updating navbar design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
       if (window.scrollY > 0) {
         document.body.style.backgroundColor("green");
       } else {
-        document.body.style.backgroundColor("transparent");
+        document.body.style.backgroundColor("blue");
       }
     };
     window.addEventListener("scroll", handleScroll);
@@ -18,12 +18,12 @@ const Navbar = () => {
   }, []);
   return (
     <>
-      <div className="flex justify-evenly py-3 mt-3">
-        <div>
-          <h1 className="font-bold text-2xl cursor-pointer">Logo</h1>
+      <div className="flex justify-evenly py-3 bg-blue-600">
+        <div className="flex items-center justify-center">
+          <h1 className="font-bold text-2xl cursor-pointer text-white">Logo</h1>
         </div>
-        <div>
-          <ul className="flex items-center justify-center gap-8 font-bold">
+        <div className="flex items-center">
+          <ul className="flex gap-8 font-bold text-white">
             {NavItems.map((value, index) => (
               <li key={index} className="hover:text-yellow-500 cursor-pointer">
                 {value}
@@ -31,7 +31,7 @@ const Navbar = () => {
             ))}
           </ul>
         </div>
-        <div>
+        <div className="flex items-center justify-center">
           <button className="font-bold  border-2 p-2 rounded-xl bg-orange-400 hover:bg-orange-300 cursor-pointer border-green-600">
             Contact Us
           </button>
