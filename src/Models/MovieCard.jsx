39f8f053baa4b506f7a50e6b9276900228b5f49e
Qamar5656 <qamar4157@gmail.com>
2025-07-18 10:36:24 +0500

    Updating the long description and display load more button

--- src/Models/MovieCard.jsx
+++ src/Models/MovieCard.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
+
 const MovieCard = ({ img, title, rating, description }) => {
   const stars = Math.round(rating); // Round to nearest integer (e.g. 7.8 → 8)
+  const [wordsLen, setWordsLen] = useState(150);
 
+  const handleWordsDisplay = () => {
+    setWordsLen(description.length);
+  };
+  const shortdescription = description.slice(0, wordsLen);
   return (
     <div className="bg-white rounded-lg shadow-md overflow-hidden cursor-pointer hover:shadow-xl">
       <img src={img} alt={title} className="w-full h-64 object-cover" />
@@ -14,7 +21,19 @@ const MovieCard = ({ img, title, rating, description }) => {
             ))}
         </div>
         <p className="text-sm text-gray-600">Rating: {rating.toFixed(1)}</p>
-        <p className="text-sm text-gray-600">Description: {description}</p>
+
+        {/* <p className="text-sm text-gray-600">Description: {description}</p> */}
+        <p className="text-sm text-gray-600">
+          {shortdescription}
+          {description.length > wordsLen && (
+            <button
+              onClick={handleWordsDisplay}
+              className="px-4 font-bold cursor-pointer"
+            >
+              Load More
+            </button>
+          )}
+        </p>
       </div>
     </div>
   );
